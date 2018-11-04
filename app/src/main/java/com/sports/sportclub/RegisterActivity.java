b72131fab1d5f860dc0014c54f5effaea20c9c7c
16301111 <994648500@qq.com>
2018-11-04 18:24:32 +0800

    fourth commit several changes: 1.change use bmob's login&register method to use retrofit2.0 to finish the same work 2.adjust the gradle dependencies to work normally.

--- app/src/main/java/com/sports/sportclub/RegisterActivity.java
+++ app/src/main/java/com/sports/sportclub/RegisterActivity.java
@@ -2,18 +2,25 @@ package com.sports.sportclub;
 
 import android.content.Intent;
 import android.graphics.Paint;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.sports.sportclub.DataModel.User;
+import com.sports.sportclub.api.BmobService;
+import com.sports.sportclub.api.Client;
 
-import cn.bmob.v3.BmobUser;
-import cn.bmob.v3.exception.BmobException;
-import cn.bmob.v3.listener.SaveListener;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -37,7 +44,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void onClickSignup(View view) {
         //注册功能的实现
-        EditText username_input = findViewById(R.id.userEmail_input);
+        EditText username_input = findViewById(R.id.username_input);
         EditText password_input = findViewById(R.id.password_input);
         EditText ensure_password_input = findViewById(R.id.ensure_password_input);
         String username = username_input.getText().toString();
@@ -49,23 +56,60 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
 
-        BmobUser new_user = new BmobUser();
-        new_user.setPassword(password);
-        new_user.setUsername(username);
-        new_user.signUp(new SaveListener<BmobUser>() {
+        //bmob内部封装的注册方法
+//        BmobUser new_user = new BmobUser();
+//        new_user.setPassword(password);
+//        new_user.setUsername(username);
+//        new_user.signUp(new SaveListener<BmobUser>() {
+//
+//            @Override
+//            public void done(BmobUser user, BmobException e) {
+//                if(e == null){
+//                    showmsg("注册成功");
+//                    BmobUser.logOut();
+//                    jump2login();
+//                }
+//                else{
+//                    showmsg(e.getMessage().toString());
+//                }
+//            }
+//        });
+
+        //创建注册用户模板
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("username",username);
+            jsonObject.put("password",password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //将json转为请求体
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json"),jsonObject.toString());
+
+        //使用retrofit发送请求
+        BmobService service = Client.retrofit.create(BmobService.class);
+        Call<ResponseBody> call = service.postUser(body);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                    if(response.code() == 201){
+                        showmsg("注册成功");
+                        jump2login();
+                    }
+                    else if(response.code() == 400) {
+                        showmsg("该用户名已存在");
+                    }
+
+            }
 
             @Override
-            public void done(BmobUser user, BmobException e) {
-                if(e == null){
-                    showmsg("注册成功");
-                    BmobUser.logOut();
-                    jump2login();
-                }
-                else{
-                    showmsg(e.getMessage().toString());
-                }
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                showmsg(t.getMessage());
             }
         });
+
     }
     //显示信息
     public void showmsg(String msg){

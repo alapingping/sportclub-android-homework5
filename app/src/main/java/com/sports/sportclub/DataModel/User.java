7ceed2846cb609e4a18fc76dab15a127ff93d932
16301111 <994648500@qq.com>
2018-11-08 20:45:15 +0800

    fourth commit several changes: 1.change use bmob's login&register method to use retrofit2.0 to finish the same work 2.adjust the gradle dependencies to work normally. 3.add retrofit client

--- app/src/main/java/com/sports/sportclub/DataModel/User.java
+++ app/src/main/java/com/sports/sportclub/DataModel/User.java
@@ -3,8 +3,6 @@ package com.sports.sportclub.DataModel;
 import cn.bmob.v3.BmobUser;
 
 /**
- * Created by 16301 on 2018/10/30 0030.
- *
  * the User Model for the app
  * include:
  * String: UserName
